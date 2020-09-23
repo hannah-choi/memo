443098648c06c9672162539320b80e752f7d5121
hannah-developer <hannahchoi.developer@gmail.com>
2020-09-23 20:04:35 +0900

    select >>> count

--- backEnd/routes/user.js
+++ backEnd/routes/user.js
@@ -8,15 +8,15 @@ router.get('/', (req,res)=>{
     })
 })
 
-router.get('/login', (req, res)=>{
-    //res.set('Set-Cookie', [`username=${req.body.username}`,`password=${req.body.password}`, `userID=${req.body.userID}`])
+router.post('/login', (req, res)=>{
     console.log(req.body)
-    db.query(`SELECT COUNT(*) FROM user WHERE email = '${req.body.email}' and password = '${req.body.password}'`, (err,rows)=>{
-        //console.log(`SELECT memo.userID, password, id, color,pageX, pageY FROM memo JOIN user ON memo.userID = user.userID WHERE memo.userID = ${parseInt(req.body.userID)}`)
-        if(rows[0] === 0){
-            console.log('invalid email or pw')
-        } else if(rows[0] === 1) {
-            res.send(rows)}
+    db.query(`SELECT COUNT(*) as count FROM user WHERE email = '${req.body.email}' and password = '${req.body.password}'`, (err,rows)=>{ 
+        if(rows[0].count === 0){
+            res.redirect('/login.html')
+        } else {
+            res.redirect('/index.html')
+            res.set('Set-Cookie', [`username=${req.body.email}`,`password=${req.body.password}`])}
+
     })
   });
 
