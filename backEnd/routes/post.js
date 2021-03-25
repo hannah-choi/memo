59b46c9e99d4c4bd3765d42720ce9988252d3138
hannah-developer <hannahchoi.developer@gmail.com>
2021-03-25 11:28:03 +0000

    modified Parameterized Queries

--- backEnd/routes/post.js
+++ backEnd/routes/post.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const db = require("../db.js");
 
 router.get("/", (req, res) => {
-    db.query(`SELECT * FROM memo`, (err, rows) => {
+    const table = ["memo"];
+    db.query(`SELECT * FROM ??`, table, (err, rows) => {
         res.send(rows);
     });
 });
 
 router.post("/create", (req, res) => {
+    const table = "memo";
+    const columns = ["color", "pageX", "pageY"];
     const params = [req.body.color, req.body.pageX, req.body.pageY];
     db.query(
-        `INSERT INTO memo(color, pageX, pageY) VALUES(?, ?, ?)`,
-        params,
+        `INSERT INTO ${table}(??) VALUES(?)`,
+        [columns, params],
         (err, rows) => {
             if (err) throw err;
             res.send({
@@ -27,50 +30,52 @@ router.post("/create", (req, res) => {
 });
 
 router.get("/color", (req, res) => {
-    const params = [req.query.color, parseInt(req.query.id)];
-    db.query(`UPDATE memo SET color = ? WHERE id = ?`, params, err => {
+    const param1 = { color: req.query.color };
+    const param2 = { id: parseInt(req.query.id) };
+    db.query(`UPDATE memo SET ? WHERE ?`, [param1, param2], err => {
         if (err) throw err;
     });
 });
 
 router.get("/position", (req, res) => {
-    const params = [req.query.pageX, req.query.pageY, req.query.id];
-    db.query(
-        "UPDATE memo SET pageX = ?, pageY = ? WHERE id = ?",
-        params,
-        err => {
-            if (err) throw err;
-            res.send("success");
-        }
-    );
+    const param1 = { pageX: req.query.pageX };
+    const param2 = { pageY: req.query.pageY };
+    const param3 = { id: req.query.id };
+    db.query("UPDATE memo SET ?, ? WHERE ?", [param1, param2, param3], err => {
+        if (err) throw err;
+        res.send("success");
+    });
 });
 
 router.put("/", (req, res) => {
-    const params = [req.body.text, parseInt(req.body.id)];
-    db.query(`UPDATE memo SET text = ? WHERE id = ?`, params, err => {
+    const postid = parseInt(req.body.id);
+    const param1 = { text: req.body.text };
+    const param2 = { id: postid };
+    db.query(`UPDATE memo SET ? WHERE ?`, [param1, param2], err => {
         if (err) throw err;
         res.send("success");
     });
 });
 
 router.get("/delete", (req, res) => {
-    const params = [parseInt(req.query.id)];
-    db.query(`DELETE FROM memo WHERE id = ?`, params, err => {
+    const params = { id: parseInt(req.query.id) };
+    db.query(`DELETE FROM memo WHERE ?`, params, err => {
         if (err) throw err;
         res.send("success");
     });
 });
 
 router.delete("/all", (req, res) => {
-    db.query(`DELETE FROM memo`, err => {
+    const columns = ["memo"];
+    db.query(`DELETE FROM ?`, columns, err => {
         if (err) throw err;
         res.send("success");
     });
 });
 
 router.get("/filter", (req, res) => {
-    const params = [req.query.color];
-    db.query(`SELECT * FROM memo WHERE COLOR = ?`, params, (err, rows) => {
+    const params = { color: req.query.color };
+    db.query(`SELECT * FROM memo WHERE ?`, params, (err, rows) => {
         if (err) throw err;
         res.send(rows);
     });
--- frontEnd/javascripts/index.js
+++ frontEnd/javascripts/index.js
@@ -1,117 +1,122 @@
-import FetchClass from './FetchClass.js'
-import MemoManager from './MemoManager.js'
-import ContextMenu from './ContextMenu.js'
-import BgMenu from './BgMenu.js'
-import PostItMenu from './PostItMenu.js'
+import FetchClass from "./FetchClass.js";
+import MemoManager from "./MemoManager.js";
+import ContextMenu from "./ContextMenu.js";
+import BgMenu from "./BgMenu.js";
+import PostItMenu from "./PostItMenu.js";
 
-const memoSection = document.querySelector('.memoSection')
-const body = document.querySelector('body')
-const memoApp = new MemoManager()
-const fetchClass = new FetchClass()
-fetchClass.getData(memoApp.updateData)
-const postItMenu = new PostItMenu(memoApp.updateData, memoSection)
-const bgMenu = new BgMenu(memoApp.updateData, memoSection)
-const filterSection = document.querySelector('.memoList')
+const memoSection = document.querySelector(".memoSection");
+const body = document.querySelector("body");
+const memoApp = new MemoManager();
+const fetchClass = new FetchClass();
+fetchClass.getData(memoApp.updateData);
+const postItMenu = new PostItMenu(memoApp.updateData, memoSection);
+const bgMenu = new BgMenu(memoApp.updateData, memoSection);
+const filterSection = document.querySelector(".memoList");
 
-function getColor(){
-    const colors = ['lightblue','blue','brightred','lightpink']
-    const index = Math.floor(Math.random() * 4)
-    console.log(colors[index])
-    return colors[index]
+function getColor() {
+    const colors = ["lightblue", "blue", "brightred", "lightpink"];
+    const index = Math.floor(Math.random() * 4);
+    return colors[index];
 }
 
-body.addEventListener('click', (e)=>{
-    switch(e.target.dataset.name){
+body.addEventListener("click", e => {
+    switch (e.target.dataset.name) {
         case "create":
-            fetchClass.createMemo(getColor(), e.clientX, e.clientY, memoApp.createMemo)
+            fetchClass.createMemo(
+                getColor(),
+                e.clientX,
+                e.clientY,
+                memoApp.createMemo
+            );
             break;
         case "deleteAll":
-            fetchClass.deleteAllMemo()
-            .then(text => {
-                if(text === 'success'){
+            fetchClass.deleteAllMemo().then(text => {
+                if (text === "success") {
                     memoApp.deleteAllMemo();
-                }}
-            )
+                }
+            });
             break;
         case "delete":
-            fetchClass.deleteMemo(+e.target.dataset.id)
-            .then(text => {
-                if(text === 'success'){
-                memoApp.deleteMemo(e.target.dataset.id)
-                }}
-            )
+            fetchClass.deleteMemo(+e.target.dataset.id).then(text => {
+                if (text === "success") {
+                    memoApp.deleteMemo(e.target.dataset.id);
+                }
+            });
             break;
         case "minimize":
-            memoApp.minimize(e.target.parentElement.parentElement)
+            memoApp.minimize(e.target.parentElement.parentElement);
             break;
         case "maximize":
-            memoApp.maximize(e.target.parentElement.parentElement)
+            memoApp.maximize(e.target.parentElement.parentElement);
             break;
         case "post":
-            memoApp.bringFront(postItMenu.targetConvert(e.target))
+            memoApp.bringFront(postItMenu.targetConvert(e.target));
             break;
         default:
-            ContextMenu.remove()
+            ContextMenu.remove();
             return;
-        }
-    })
+    }
+});
 
-memoSection.addEventListener('contextmenu', (e)=>{
-    e.preventDefault(); 
-    let clickedItem = ""
-    if(e.target.tagName === "HEADER"){
-        clickedItem = e.target.parentElement
-        postItMenu.rightButtonClick(clickedItem, e.clientY, e.clientX)
-    } else if (e.target.tagName === "TEXTAREA" || e.target.tagName === "ARTICLE") {
-        clickedItem = e.target.parentElement.parentElement.parentElement
-        postItMenu.rightButtonClick(clickedItem, e.clientY, e.clientX)
-    } 
-    else if (e.target.className === "memoSection"){
+memoSection.addEventListener("contextmenu", e => {
+    e.preventDefault();
+    let clickedItem = "";
+    if (e.target.tagName === "HEADER") {
+        clickedItem = e.target.parentElement;
+        postItMenu.rightButtonClick(clickedItem, e.clientY, e.clientX);
+    } else if (
+        e.target.tagName === "TEXTAREA" ||
+        e.target.tagName === "ARTICLE"
+    ) {
+        clickedItem = e.target.parentElement.parentElement.parentElement;
+        postItMenu.rightButtonClick(clickedItem, e.clientY, e.clientX);
+    } else if (e.target.className === "memoSection") {
         clickedItem = e.target;
-        bgMenu.rightButtonClick(e.clientY, e.clientX)
+        bgMenu.rightButtonClick(e.clientY, e.clientX);
     } else {
         return;
     }
-})
+});
 
-memoSection.addEventListener('change', ({target})=>{
-    if (target.tagName ==! "TEXTAREA"){
+memoSection.addEventListener("change", ({ target }) => {
+    if (target.tagName == !"TEXTAREA") {
         return;
     }
-    memoApp.updateMemo(target.dataset.id, target.value); 
+    memoApp.updateMemo(target.dataset.id, target.value);
     fetchClass.updateMemo(target.dataset.id, target.value);
-}) 
+});
 
-memoSection.addEventListener('dragstart',(e)=>{
-    if(e.target.className ==! 'post'){
-        return;
-    } 
-    memoApp.dragStart(e)
-})
-
-memoSection.addEventListener('dragover',(e)=>{
-    e.preventDefault();
-    if(e.target.className ==! 'post'){
+memoSection.addEventListener("dragstart", e => {
+    if (e.target.className == !"post") {
         return;
     }
-})
+    memoApp.dragStart(e);
+});
 
-memoSection.addEventListener('drop', (e)=>{
+memoSection.addEventListener("dragover", e => {
+    e.preventDefault();
+    if (e.target.className == !"post") {
+        return;
+    }
+});
+
+memoSection.addEventListener("drop", e => {
     e.preventDefault();
     const id = e.dataTransfer.getData("text");
-    const selected = document.querySelector(`[data-id="${id}"]`);   
-    const pageXValue = e.pageX-200 - memoApp.shiftX + "px";
+    const selected = document.querySelector(`[data-id="${id}"]`);
+    const pageXValue = e.pageX - 200 - memoApp.shiftX + "px";
     const pageYValue = e.pageY - memoApp.shiftY + "px";
     memoApp.drop(selected, pageXValue, pageYValue);
     fetchClass.pageUpdate(selected, pageXValue, pageYValue, memoApp.updateData);
 });
 
-filterSection.addEventListener('click', ({target})=>{
-    if(target.tagName === 'SECTION' || target.tagName === 'UL'){
+filterSection.addEventListener("click", ({ target }) => {
+    if (target.tagName === "SECTION" || target.tagName === "UL") {
         return;
-    } 
-    if (target.dataset.name === 'all'){
-        fetchClass.getData(memoApp.filterData)
+    }
+    if (target.dataset.name === "all") {
+        fetchClass.getData(memoApp.filterData);
     } else {
-        fetchClass.colorFilter(target.dataset.name, memoApp.filterData)}
+        fetchClass.colorFilter(target.dataset.name, memoApp.filterData);
+    }
 });
